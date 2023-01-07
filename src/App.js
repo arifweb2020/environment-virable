80a13739c2c7554efb184c616d0a369e96891c4f
Md Arif Hussain <67943567+arifweb2020@users.noreply.github.com>
2023-01-07 16:26:29 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 function App() {
   console.log({ REACT_APP_API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT });
   return <div>
+    <h1>npm run build-prod  check it from packge.json file</h1>
+  <h1>npm run build-qa  check it from packge.json file</h1>
+  <h1>run build app : npx serve -s build           ---- run in local browser</h1> 
     <h1>install live server : npm i -g serve</h1>
-   <h1>run build app : npx serve -s build</h1> 
+   
     Home
     </div>;
 }
